6f6d752dda6e2fe0256c246ac05c284b75ebc64f
maneth <manethgamage654@gmail.com>
2024-03-06 20:27:25 +0530

    code

--- Utils.py
+++ Utils.py
@@ -2,28 +2,15 @@ import pandas as pd
 import random
 from q_table import Q_table
 from agent import Agent
-import embedding
-
-
+from embedding import keyword_matching
 
 
 def parameters():
     config = {}
-    config['log_file'] = ''
-    config['epochs'] = 500
     config['learning_rate'] = 0.2
-    config['l2_reg'] = 0.1
-    config['num_episodes_warm'] = 100
-    config['train_freq_warm'] = 100
-    config['num_episodes'] = 100
-    config['train_freq'] = 10
     config['epsilon'] = 0.2
     config['epsilon_f'] = 0.05
     config['epsilon_epoch_f'] = 20
-    config['dqn_hidden_size'] = 60
-    config['gamma'] = 0.9
-    config['prob_no_intent'] = 0.5
-    config['buffer_size'] = 10000
     return config
 
 
@@ -41,12 +28,11 @@ def filter_and_dropna(file_path):
 
 
 def load_data():
-    file = 'GS_short_corrected.csv'
-    df = pd.read_csv(file)
+    file = 'query_responses.csv'
+    df = filter_and_dropna(file)
 
     df.dropna(subset=['utterance'], how='all', inplace=True)
     df.dropna(subset=['response'], how='all', inplace=True)
-    df = filter_and_dropna(file)
     print(df)
     map_index2action, map_action2index = actions_to_dict(df)
     # print(map_index2action)
@@ -54,11 +40,6 @@ def load_data():
     # print(map_index2state)
     feedback = df['feedback']
     similarity = df['confidence']
-
-
-    mapping = get_mapping(df)
-    q_tab = Q_table(mapping)
-    populate_q_table(df,mapping,q_tab)
 
     mapping = {}
     mapping['index2action'] = map_index2action
@@ -68,15 +49,8 @@ def load_data():
     mapping['feedback'] = feedback
     mapping['confidence'] = similarity
 
-    query_state_text = "where is the printshop men?"
-    most_similar_state, highest_score = embedding.keyword_matching(query_state_text, mapping)
-    if most_similar_state is None:
-        print("No similar state found.")
-    else:
-        print("Most similar state:", mapping['index2state'][most_similar_state])
-        print("Highest similarity score:", highest_score)
-        agent = Agent(mapping, q_tab)
-        agent.select_action(mapping['index2state'][most_similar_state])
+    # similar_state, score = embedding.keyword_matching(query, mapping)
+    # print(similar_state)
 
     return mapping,df
 
@@ -132,19 +106,28 @@ def feedback_to_dict(df):
     return map_index2feedback, map_feedback2index
 
 
-def get_mapping(df):
+def get_mapping(df,q_table):
     try:
+        new_states, new_actions = {}, {}
+        for index, row in df.iterrows():
+            state = row['utterance']
+            action = row['response']
+            if state not in q_table:
+                if state not in new_states:
+                    new_states[state] = len(new_states)
+                if action not in new_actions:
+                    new_actions[action] = len(new_actions)
         map_index2action, map_action2index = actions_to_dict(df)
         map_index2state, map_state2index = state_to_dict(df)
         feedback = df['feedback']
         similarity = df['confidence']
 
-
+        # Merge new states and actions into existing mapping
         mapping = {}
-        mapping['index2action'] = map_index2action
-        mapping['action2index'] = map_action2index
-        mapping['index2state'] = map_index2state
-        mapping['state2index'] = map_state2index
+        mapping['index2action'] = {**map_index2action, **new_actions}
+        mapping['action2index'] = {**map_action2index, **{v: k for k, v in new_actions.items()}}
+        mapping['index2state'] = {**map_index2state, **new_states}
+        mapping['state2index'] = {**map_state2index, **{v: k for k, v in new_states.items()}}
         mapping['feedback'] = feedback
         mapping['confidence'] = similarity
 
@@ -162,8 +145,8 @@ def populate_q_table(conv_dict, mapping, Q_tab):
     feedback = mapping['feedback']
     confidence_values = mapping['confidence']
 
-    print('States:', list(map_state2index.keys()))
-    print('Actions:', list(map_action2index.keys()))
+    # print('States:', list(map_state2index.keys()))
+    # print('Actions:', list(map_action2index.keys()))
 
     for index, row in conv_dict.iterrows():
         state_index = map_state2index[row['utterance']]
@@ -176,13 +159,9 @@ def populate_q_table(conv_dict, mapping, Q_tab):
 
         Q_tab.add(new_q_value, state_index, action_index)
 
+
     for state, row in zip(map_state2index.keys(), Q_tab.Q):
         print(state, ' '.join([str(int(value)) for value in row]))
 
 
-
-
-
-load_data()
-
-
+# load_data()
